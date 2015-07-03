2af9b5646331de4795cb48089d2f295c05c1564e
262Hz <262hz@MacBook-Pro.local>
2015-07-03 00:22:04 -0500

    Added project for 2.1.5

---
+++ Uber 2.1.5/Uber Unit 2-01/ViewController.m
@@ -0,0 +1,55 @@
+//
+//  ViewController.m
+//  Uber Unit 2-01
+//
+//  Created by 262Hz on 7/2/15.
+//  Copyright (c) 2015 Thinkful. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) UISlider *redSlider;
+@property (weak, nonatomic) UISlider *greenSlider;
+@property (weak, nonatomic) UISlider *blueSlider;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    CGFloat height = 50;
+    CGFloat width = self.view.frame.size.width;
+    UISlider *redSlider = [[UISlider alloc] initWithFrame:CGRectMake(0, 20, width, height)];
+    UISlider *greenSlider = [[UISlider alloc] initWithFrame:CGRectMake(0, 70, width, height)];
+    UISlider *blueSlider = [[UISlider alloc] initWithFrame:CGRectMake(0, 120, width, height)];
+    redSlider.tintColor = [UIColor redColor];
+    greenSlider.tintColor = [UIColor greenColor];
+    blueSlider.tintColor = [UIColor blueColor];
+    [self.view addSubview:redSlider];
+    [self.view addSubview:greenSlider];
+    [self.view addSubview:blueSlider];
+    self.redSlider = redSlider;
+    self.greenSlider = greenSlider;
+    self.blueSlider = blueSlider;
+    
+    [self updateBackgroundColor];
+    
+    [self.redSlider addTarget:self action:@selector(updateBackgroundColor) forControlEvents:UIControlEventValueChanged];
+    [self.greenSlider addTarget:self action:@selector(updateBackgroundColor) forControlEvents:UIControlEventValueChanged];
+    [self.blueSlider addTarget:self action:@selector(updateBackgroundColor) forControlEvents:UIControlEventValueChanged];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)updateBackgroundColor {
+    self.view.backgroundColor = [UIColor colorWithRed:self.redSlider.value green:self.greenSlider.value blue:self.blueSlider.value alpha:1];
+}
+
+@end
---
+++ Uber 2.1.5/Uber Unit 2-01Tests/Uber_Unit_2_01Tests.m
@@ -0,0 +1,40 @@
+//
+//  Uber_Unit_2_01Tests.m
+//  Uber Unit 2-01Tests
+//
+//  Created by 262Hz on 7/2/15.
+//  Copyright (c) 2015 Thinkful. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Uber_Unit_2_01Tests : XCTestCase
+
+@end
+
+@implementation Uber_Unit_2_01Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
