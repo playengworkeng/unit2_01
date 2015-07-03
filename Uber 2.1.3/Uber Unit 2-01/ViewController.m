509588c431b1587d6b313d89955a1d24270ba65c
262Hz <262hz@MacBook-Pro.local>
2015-07-02 23:22:55 -0500

    Added project for 2.1.3

---
+++ Uber 2.1.3/Uber Unit 2-01/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  Uber Unit 2-01
+//
+//  Created by 262Hz on 7/2/15.
+//  Copyright (c) 2015 Thinkful. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithRed:0.18 green:0.51 blue:0.40 alpha:1];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, self.view.frame.size.width, 44)];
+    label.text = @"Hello, World!";
+    label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:label];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
