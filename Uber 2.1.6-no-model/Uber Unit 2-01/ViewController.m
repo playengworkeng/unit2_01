f257c1b5ccc12ed2135cebbb3d8849d0d91962c6
262Hz <262hz@MacBook-Pro.local>
2015-07-03 18:00:20 -0500

    Uploading 2.1.6 project

---
+++ Uber 2.1.6-no-model/Uber Unit 2-01/ViewController.m
@@ -0,0 +1,96 @@
+//
+//  ViewController.m
+//  Uber Unit 2-01
+//
+//  Created by 262Hz on 7/2/15.
+//  Copyright (c) 2015 Thinkful. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Masonry.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) UISlider *celciusSlider;
+@property (weak, nonatomic) UILabel *celciusLabel;
+@property (weak, nonatomic) UILabel *fahrenheitLabel;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor colorWithHue:0.6 saturation:1.0 brightness:0.18 alpha:1.0];
+    
+    UISlider *celciusSlider = [UISlider new];
+    UILabel *celciusLabel = [UILabel new];
+    UILabel *fahrenheitLabel = [UILabel new];
+    [self.view addSubview:celciusSlider];
+    [self.view addSubview:celciusLabel];
+    [self.view addSubview:fahrenheitLabel];
+    self.celciusSlider = celciusSlider;
+    self.celciusLabel = celciusLabel;
+    self.fahrenheitLabel = fahrenheitLabel;
+    
+    UIColor *color = [UIColor colorWithHue:0.22 saturation:1.0 brightness:0.9 alpha:1.0];
+    self.celciusSlider.tintColor = color;
+    self.celciusSlider.minimumValue = -100;
+    self.celciusSlider.maximumValue = 100;
+    [self.celciusSlider addTarget:self action:@selector(updateTemperatureLabels) forControlEvents:UIControlEventValueChanged];
+    
+    self.celciusLabel.text = @"0°C";
+    self.fahrenheitLabel.text = @"32°F";
+    
+    [self.celciusLabel setFont:[UIFont boldSystemFontOfSize:60]];
+    self.celciusLabel.textColor = color;
+    self.celciusLabel.textAlignment = NSTextAlignmentRight;
+    [self.fahrenheitLabel setFont:[UIFont boldSystemFontOfSize:60]];
+    self.fahrenheitLabel.textColor = color;
+    self.fahrenheitLabel.textAlignment = NSTextAlignmentRight;
+    
+    CGFloat leadingOffset = 20;
+    CGFloat trailingOffset = -20;
+    CGFloat verticalSpace = 20;
+    
+    [self.celciusSlider mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(self.view).offset(-verticalSpace);
+        make.leading.equalTo(self.view.mas_leading).offset(leadingOffset);
+        make.trailing.equalTo(self.view.mas_trailing).offset(trailingOffset);
+    }];
+    
+    [self.celciusLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        UIView *topLayoutGuide = (id)self.topLayoutGuide;
+        make.top.equalTo(topLayoutGuide.mas_bottom).offset(verticalSpace);
+        make.leading.equalTo(self.view.mas_leading).offset(leadingOffset);
+        make.trailing.equalTo(self.view.mas_trailing).offset(trailingOffset);
+    }];
+    
+    [self.fahrenheitLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.celciusLabel.mas_bottom).offset(verticalSpace);
+        make.leading.equalTo(self.view.mas_leading).offset(leadingOffset);
+        make.trailing.equalTo(self.view.mas_trailing).offset(trailingOffset);
+    }];
+    
+    [self updateTemperatureLabels];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
+- (void)updateTemperatureLabels {
+    float celcius = self.celciusSlider.value;
+    float fahrenheit = celcius * 1.8 + 32;
+    
+    self.celciusLabel.text = [NSString stringWithFormat:@"%.02f°C", celcius];
+    self.fahrenheitLabel.text = [NSString stringWithFormat:@"%.02f°F", fahrenheit];
+}
+
+@end
